7feedb2b9fa0d76d1328f40da33582cff657ce6c
Jie LEBLOND <jleblond@e3r10p6.42.fr>
2019-11-13 12:21:51 +0100

    flag -v added, prog checker works well

--- srcs/execute_instructions.c
+++ srcs/execute_instructions.c
@@ -12,73 +12,69 @@
 
 #include "push_swap.h"
 
-int			init_stack_succeed(int *args, int args_len, t_stack **stk)
+t_stack			*init_stack(int *args, int nb_args)
 {
-	*stk = (t_stack*)malloc(sizeof(t_stack));
+	t_stack		*stk;
+
+	stk = (t_stack*)malloc(sizeof(t_stack));
 	if (stk == NULL)
-		return (FALSE);
-	(*stk)->a = args;
-	(*stk)->a_len = args_len;
-	(*stk)->b = (int*)malloc(sizeof(int) * args_len);
-	if ((*stk)->b == NULL)
-		return (FALSE);
-	(*stk)->b_len = 0;
-	return (TRUE);
-}
-
-void		show_stack(t_stack *stk)
-{
-	int		max;
-	int		i;
-	int		j;
-
-	i = stk->a_len;
-	j = stk->b_len;
-	max = (i > j ? i : j);
-	while (max - 1 >= 0)
+		return (NULL);
+	stk->a = args;
+	stk->a_len = nb_args;
+	stk->b = (int*)malloc(sizeof(int) * nb_args);
+	if (stk->b == NULL)
 	{
-		if (i == max && j == max)
-			ft_printf("%d   %d\n", stk->a[max - 1], stk->b[max - 1]);
-		else if (i == max && j != max)
-			ft_printf("%d    \n", stk->a[max - 1]);
-		else if (i != max && j == max)
-			ft_printf("     %d\n", stk->b[max - 1]);
-		i == max ? i-- : 0;
-		j == max ? j-- : 0;
-		max--;
+		free(stk);
+		return (NULL);
 	}
-	ft_printf("-----\na   b\n");
+	stk->b_len = 0;
+	return (stk);
 }
 
-t_stack		*execute_instructions(t_list *ins_lst, int *args, int args_len)
+t_stack		*loop_instructions(t_list *ins_lst, t_stack *stk, char *flags)
 {
-	t_stack	*stk;
 	t_funs	funs[INSTRUCTION_NB] = {{SA, sa}, {SB, sb}, {SS, ss}, {PA, pa},
 	{PB, pb}, {RA, ra}, {RB, rb}, {RR, rr}, {RRA, rra}, {RRB, rrb}, {RRR, rrr}
 	};
 	int		index;
 
 	index = 0;
-	if (init_stack_succeed(args, args_len, &stk))
+	if ((*flags) & V_FLAG)
 	{
-		ft_printf("original stack:\n");
+		ft_printf("Init stack:\n");
 		show_stack(stk);
-		while (ins_lst != NULL)
-		{
-			while (index < INSTRUCTION_NB)
-			{
-				if (funs[index].ins == *(int*)(ins_lst->content))
-				{
-					funs[index].f(&stk);
-					show_stack(stk);
-				}
-				index++;
-			}
-			ins_lst = ins_lst->next;
-			index = 0;
-		}
 	}
-	else
+	while (ins_lst != NULL)
+	{
+		while (index < INSTRUCTION_NB)
+		{
+			if (funs[index].ins == *(int*)(ins_lst->content))
+			{
+				funs[index].f(&stk);
+				(*flags) & V_FLAG ? show_stack(stk) : 0;
+			}
+			index++;
+		}
+		ins_lst = ins_lst->next;
+		index = 0;
+	}
+	return (stk);
+}
+
+t_stack		*execute_instructions(t_list *ins_lst, int *args, int argc, char *flags)
+{
+	t_stack	*stk;
+	int		nb_args;
+
+	nb_args = argc -1;
+	if ((*flags) & V_FLAG)
+		nb_args--;
+	stk = init_stack(args, nb_args);
+	if (stk == NULL)
+	{
 		ft_putendl_fd("Init stack failed!", 2);
+		return (NULL);
+	}
+	stk = loop_instructions(ins_lst, stk, flags);
 	return (stk);
 }
